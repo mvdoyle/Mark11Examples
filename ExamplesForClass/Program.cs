612973b1ed712770490e83217a73c5fb70641c34
mvdoyle <mdoyle@truecoders.io>
2020-04-09 13:16:17 -0500

    classes exercise

---
+++ ExamplesForClass/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ExamplesForClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var michaelsTV = new TV();
+
+            michaelsTV.Is4k = true;
+            michaelsTV.Brand = "Samsung";
+            michaelsTV.IsFlatScreen = true;
+            michaelsTV.ScreenSize = 55;
+
+            var brettsTV = new TV();
+            brettsTV.Is4k = false;
+            brettsTV.IsFlatScreen = false;
+            brettsTV.ScreenSize = 19;
+            brettsTV.IsSmart = false;
+            brettsTV.Brand = "Magnavox";
+
+            brettsTV.PrintInfo();
+        }
+    }
+}
---
+++ ExamplesForClass/TV.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace ExamplesForClass
+{
+    public class TV
+    {
+        public TV()
+        {
+        }
+
+        //properties are class members
+        public int ScreenSize { get; set; }
+        public bool IsFlatScreen { get; set; }
+        public bool Is4k { get; set; }
+        public string Brand { get; set; }
+        public bool IsSmart { get; set; }
+
+        //methods are class members
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Brand name: {Brand}, Screen size:{ScreenSize}");
+        }
+    }
+}
